926bb2f3ac013ad197901a19859738b8f2eb0908
user1 <user1@benjamin-ThinkPad-T450s>
2016-07-05 11:29:47 +0200

    init spring auth

--- src/main/java/edu/kit/scc/filesystem/FilesystemConfiguration.java
+++ src/main/java/edu/kit/scc/filesystem/FilesystemConfiguration.java
@@ -7,7 +7,7 @@
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
-package edu.kit.scc;
+package edu.kit.scc.filesystem;
 
 import org.indigo.cdmi.BackEndException;
 import org.indigo.cdmi.BackendCapability;
